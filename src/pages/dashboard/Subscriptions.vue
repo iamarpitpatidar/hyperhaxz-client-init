a9224dc5935405e67c9427778b86703019266505
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-06 02:45:45 +0530

    added subscription page

--- src/pages/dashboard/Subscriptions.vue
+++ src/pages/dashboard/Subscriptions.vue
@@ -10,25 +10,31 @@
                   scope="col"
                   class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  UID
+                  ID
                 </th>
                 <th
                   scope="col"
                   class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  Username
+                  Code
                 </th>
                 <th
                   scope="col"
                   class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  Roles
+                  Expiry
                 </th>
                 <th
                   scope="col"
                   class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  Status
+                  Plan
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Created By
                 </th>
                 <th
                   scope="col"
@@ -40,62 +46,32 @@
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
               <tr
-                v-for="person in people"
-                :key="person.email"
+                v-for="subscription in subscriptions"
+                :key="subscription.code"
               >
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {{ person.uid }}
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-600">
+                  {{ subscription._id }}
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="flex items-center">
-                    <div>
-                      <div class="text-sm font-medium text-gray-900">
-                        {{ person.username }}
-                      </div>
-                      <div class="text-sm text-gray-500">
-                        {{ person.email }}
-                      </div>
-                    </div>
-                  </div>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-600">
+                  {{ subscription.code }}
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <span
-                    v-for="role in person.roles"
-                    :key="role"
-                    :class="['px-2', 'mr-2', 'inline-flex', 'text-xs', 'leading-5', 'font-semibold', 'rounded-full', role === `Admin` ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800']"
-                  >
-                    {{ role }}
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-600">
+                  {{ subscription.expiry }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-600">
+                  <span :class="['px-2', 'inline-flex', 'text-xs', 'leading-5', 'font-semibold', 'rounded-full', isActive(subscription) ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800']">
+                    {{ subscription.plan }}
                   </span>
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <span :class="['px-2', 'inline-flex', 'text-xs', 'leading-5', 'font-semibold', 'rounded-full', isActive(person) ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800']">
-                    {{ person.status }}
-                  </span>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-600">
+                  {{ subscription.creator }}
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
                   <a
                     href="#"
                     class="text-indigo-600 hover:text-indigo-900 mr-2"
                   >
-                    <chip class="inline-block h-6 w-6" />
-                  </a>
-                  <a
-                    href="#"
-                    class="text-indigo-600 hover:text-indigo-900 mx-2"
-                  >
-                    <ban class="inline-block h-6 w-6" />
-                  </a>
-                  <a
-                    href="#"
-                    class="text-indigo-600 hover:text-indigo-900 mx-2"
-                  >
-                    <admin class="inline-block h-6 w-6" />
-                  </a>
-                  <a
-                    href="#"
-                    class="text-indigo-600 hover:text-indigo-900 ml-2"
-                  >
-                    <remove class="inline-block h-6 w-6" />
+                    <delete class="inline-block h-6 w-6" />
                   </a>
                 </td>
               </tr>
@@ -109,56 +85,50 @@
 </template>
 
 <script>
-import Chip from '~/assets/icons/chip.svg'
-import Ban from '~/assets/icons/ban.svg'
-import Admin from '~/assets/icons/admin.svg'
-import Remove from '~/assets/icons/remove.svg'
+import Delete from '~/assets/icons/trash.svg'
 import Pagination from '~/components/Pagination'
 
-const people = [
+const subscriptions = [
   {
-    uid: 1,
-    username: 'iamarpit',
-    status: 'Active',
-    roles: ['Admin', 'Seller'],
-    email: 'arpit.ptdr@hotmail.com'
+    _id: 1,
+    code: 'SylWt-uYUpM-iBtwX-wwqJo-bza5S',
+    expiry: '30 Jun 2021',
+    creator: 'iamarpit',
+    plan: 'Pro'
   },
   {
-    uid: 2,
-    username: 'hyper',
-    status: 'Banned',
-    roles: ['Admin'],
-    email: 'hyper@gmail.com'
+    _id: 2,
+    code: 'U5cpU-UlF9P-yPVbB-itQWR-b3NIA',
+    expiry: '10 Apr 2022',
+    creator: 'hyper',
+    plan: 'Pro'
   },
   {
-    uid: 3,
-    username: 'someguy123',
-    status: 'Active',
-    roles: ['Admin', 'someRole'],
-    email: 'someguy@live.com'
+    _id: 3,
+    code: 'pyq75-EcLI4-0iHi8-9t7hN-O52z2',
+    expiry: '12 Dec 2022',
+    creator: 'someguy123',
+    plan: 'Free'
   },
   {
-    uid: 4,
-    username: 'schlumped',
-    status: 'Active',
-    roles: ['seller'],
-    email: 'schlumped@live.com'
+    _id: 4,
+    code: 'Yr12X-LdrtS-rmj6M-8klCr-uKwKK',
+    expiry: '15 Aug 2024',
+    creator: 'schlumped',
+    plan: 'Plus'
   },
   {
-    uid: 5,
-    username: 'boss',
-    status: 'Active',
-    roles: ['Admin'],
-    email: 'boss@google.com'
+    _id: 5,
+    code: 'HpJEQ-DUAKf-mTOpi-7JeGg-ZpR4G',
+    expiry: '23 Nov 2021',
+    creator: 'boss',
+    plan: 'Free'
   }
 ]
 
 export default {
   components: {
-    Ban,
-    Chip,
-    Admin,
-    Remove,
+    Delete,
     Pagination
   },
   metaInfo: {
@@ -166,12 +136,12 @@ export default {
   },
   data () {
     return {
-      people
+      subscriptions
     }
   },
   methods: {
-    isActive (person) {
-      return person.status === 'Active'
+    isActive (subscription) {
+      return subscription.plan === 'Pro' || subscription.plan === 'Plus'
     }
   }
 }
