5d9c9bec268aae9be52001fd19c45b624bfbdb02
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-24 23:00:05 +0530

    deps: removed unused dependencies

--- src/main.js
+++ src/main.js
@@ -1,26 +1,21 @@
-// Import global styles
 import '~/assets/styles/index.css'
 import DefaultLayout from '~/layouts/Default'
 import Dashboard from '~/layouts/Dashboard'
 
 import Vuex from 'vuex'
-// import { InlineSvgPlugin } from 'vue-inline-svg'
 
 import { actions, getters, initiateState, mutations } from './store'
 import { menu } from './store/modules'
 
 export default function (Vue, { router, appOptions }) {
   Vue.use(Vuex)
-  // Vue.use(InlineSvgPlugin)
 
   Vue.component('Layout', DefaultLayout)
   Vue.component('Dashboard', Dashboard)
 
-  // Router settings
   router.options.linkActiveClass = 'e4w'
   router.options.linkExactActiveClass = 'xr5t'
 
-  // registering the store
   appOptions.store = new Vuex.Store({
     state: initiateState(),
     mutations: mutations,
