a1ab77437693bf00d17969899948039bc9ddad02
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-05-24 21:16:22 +0530

    fix: outline-none on buttons and logo

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -3,15 +3,26 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex items-center justify-between h-16">
         <div class="text-2xl font-semibold text-gray-800">
-          <g-link to="/">HyperHaxZ</g-link>
+          <g-link
+            class="focus:outline-none"
+            to="/"
+          >
+            HyperHaxZ
+          </g-link>
         </div>
         <div>
-          <button class="py-1 px-3 m-1 rounded-md hover:bg-gray-200">
-            <g-link to="/auth/login">Login</g-link>
-          </button>
-          <button class="py-1 px-3 m-1 rounded-md bg-white hover:bg-gray-100">
-            <g-link to="/auth/register">Register</g-link>
-          </button>
+          <g-link
+            to="/auth/login"
+            class="p-0.5 px-3 m-1 rounded-md hover:bg-gray-200 focus:outline-none"
+          >
+            Login
+          </g-link>
+          <g-link
+            to="/auth/register"
+            class="p-0.5 px-3 m-1 rounded-md bg-white hover:bg-gray-100 focus:outline-none"
+          >
+            Register
+          </g-link>
         </div>
       </div>
     </div>
