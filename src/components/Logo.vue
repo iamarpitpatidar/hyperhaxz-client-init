2c759cd61f2f108875199869c764649bb649d2f2
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-06-04 19:25:26 +0530

    minor UI edits

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
   <g-link to="/dashboard/">
-    <h2 class="text-3xl font-semibold text-blue-500 w-min mx-auto">HyperHaxZ</h2>
+    <h2 class="text-3xl select-none font-semibold text-gray-600 w-min mx-auto">HyperHaxZ</h2>
   </g-link>
 </template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,32 +2,32 @@
   <nav class="mt-6">
     <nav-link to="/dashboard/">
       <home-icon class="h-5 w-5" />
-      <span class="mx-4 font-medium text-blue-500">Home</span>
+      <span class="mx-4 font-medium text-gray-400">Home</span>
     </nav-link>
     <nav-link to="/dashboard/users/">
       <users-icon class="h-5 w-5" />
-      <span class="mx-4 font-medium text-blue-500">Users</span>
+      <span class="mx-4 font-medium text-gray-400">Users</span>
     </nav-link>
     <nav-link to="/dashboard/invites/">
       <invite-icon class="h-5 w-5" />
-      <span class="mx-4 font-medium text-blue-500">Invite Codes</span>
+      <span class="mx-4 font-medium text-gray-400">Invite Codes</span>
     </nav-link>
     <nav-link to="/dashboard/fraud/">
       <fraud-icon class="h-5 w-5" />
-      <span class="mx-4 font-medium text-blue-500">Fraud</span>
+      <span class="mx-4 font-medium text-gray-400">Fraud</span>
     </nav-link>
     <nav-link to="/dashboard/sellers/">
       <sellers-icon class="h-5 w-5" />
-      <span class="mx-4 font-medium text-blue-500">Sellers</span>
+      <span class="mx-4 font-medium text-gray-400">Sellers</span>
     </nav-link>
     <hr class="my-6 border-t-2 border-blue-500">
     <nav-link to="/dashboard/profile/">
       <profile-icon class="h-5 w-5" />
-      <span class="mx-4 font-medium text-blue-500">Profile</span>
+      <span class="mx-4 font-medium text-gray-400">Profile</span>
     </nav-link>
     <nav-link to="/dashboard/subscriptions/">
       <subscription-icon class="h-5 w-5" />
-      <span class="mx-4 font-medium text-blue-500">Subscriptions</span>
+      <span class="mx-4 font-medium text-gray-400">Subscriptions</span>
     </nav-link>
   </nav>
 </template>
